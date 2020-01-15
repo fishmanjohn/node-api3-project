58c2fd8124d7c5392c7079e95512c2a6e98ceda9
fishmanjohn <john@emmaandjohnbyvan.com>
2020-01-15 15:48:42 -0800

    one down many to go

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger)
+const userRouter = require('./users/userRouter')
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
+server.use('/api/user',userRouter)
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}` 
+  )
+
+  next();
+}
 
 module.exports = server;
